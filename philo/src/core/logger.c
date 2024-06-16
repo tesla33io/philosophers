99fc79a9705e39a02b3e568ef45b54ea083d8633
tesla33io <artemstavropolov@gmail.com>
2024-06-16 19:16:01 +0200

    chore: fixing data races and other stuff 1/

--- philo/src/core/logger.c
+++ philo/src/core/logger.c
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 15:54:00 by astavrop          #+#    #+#             */
-/*   Updated: 2024/06/15 21:49:41 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/06/16 17:09:14 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@
 
 void	log_state(t_philo *philo)
 {
-	if (!philo || philo->state == DEAD || philo->data->someone_died)
-		return ;
 	pthread_mutex_lock(&philo->data->print_lock);
+	if (!philo || philo->state == DEAD || philo->data->someone_died)
+	{
+		pthread_mutex_unlock(&philo->data->print_lock);
+		return ;
+	}
 	printf("%li ", timestamp() - philo->data->start_time);
 	printf("%d is ", philo->id + 1);
 	if (philo->state == EATING)
@@ -35,9 +38,12 @@ void	log_state(t_philo *philo)
 
 void	log_action(t_philo *philo, char *act)
 {
-	if (!philo || !act || philo->data->someone_died)
-		return ;
 	pthread_mutex_lock(&philo->data->print_lock);
+	if (!philo || !act || philo->data->someone_died)
+	{
+		pthread_mutex_unlock(&philo->data->print_lock);
+		return ;
+	}
 	printf("%li ", timestamp() - philo->data->start_time);
 	printf("%d %s\n", philo->id + 1, act);
 	pthread_mutex_unlock(&philo->data->print_lock);
--- philo/src/core/routine.c
+++ philo/src/core/routine.c
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 15:59:29 by astavrop          #+#    #+#             */
-/*   Updated: 2024/06/15 22:28:13 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/06/16 18:18:43 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,18 @@ void	mealtime(t_philo *philo)
 {
 	if (philo->data->someone_died)
 		return ;
-	pthread_mutex_lock(&(*philo->left_fork));
-	log_action(philo, FORK_TAKEN_MSG);
-	pthread_mutex_lock(&(*philo->right_fork));
+	if (philo->id % 2 == 0)
+	{
+        pthread_mutex_lock(&(*philo->left_fork));
+        log_action(philo, FORK_TAKEN_MSG);
+        pthread_mutex_lock(&(*philo->right_fork));
+    }
+	else
+	{
+        pthread_mutex_lock(&(*philo->right_fork));
+        log_action(philo, FORK_TAKEN_MSG);
+        pthread_mutex_lock(&(*philo->left_fork));
+    }
 	log_action(philo, FORK_TAKEN_MSG);
 	philo->state = EATING;
 	log_state(philo);
@@ -66,7 +75,30 @@ void	starvation_time(t_philo *philo)
 		pthread_mutex_unlock(&philo->data->death_lock);
 		return ;
 	}
-	pthread_mutex_unlock(&philo->data->death_lock);
 	log_action(philo, DIED_MSG);
 	philo->data->someone_died = true;
+	pthread_mutex_unlock(&philo->data->death_lock);
 }
+
+/*
+static void	take_forks(t_philo *philo)
+{
+	int	id_prev;
+	int	id_next;
+
+	id_prev = philo->id - 1;
+	id_next = philo->id + 1;
+	if (id_prev < 0)
+		id_prev = philo->data->philo_n_fork_num - id_prev;
+	if (id_next > philo->data->philo_n_fork_num)
+		id_next = id_next - philo->data->philo_n_fork_num;
+	if (philo->data->philos[id_prev]->state != EATING
+			&& philo->data->philos[id_next]->state != EATING)
+	{
+		pthread_mutex_lock(&(*philo->left_fork));
+        log_action(philo, FORK_TAKEN_MSG);
+		pthread_mutex_lock(&(*philo->right_fork));
+        log_action(philo, FORK_TAKEN_MSG);
+	}
+}
+*/
