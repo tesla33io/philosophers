eb3103b542c0f3db8f9a833cbbb49dbb7c8e8007
astavrop <artemstavropolov@gmail.com>
2024-08-15 17:14:21 +0200

    Chore: add some colors

--- philo/src/utils/usage.c
+++ philo/src/utils/usage.c
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 20:06:26 by astavrop          #+#    #+#             */
-/*   Updated: 2024/06/08 20:17:04 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/08/15 16:01:32 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void	print_usage(void)
 {
-	printf("\nUsage: philo <* number_of_philosophers> ");
-	printf("<* time_to_die> <* time_to_eat> <* time_to_sleep> ");
-	printf("<? number_of_times_each_philosopher_must_eat>\n");
-	printf("\n\t<*> - mandatory\n\t<?> - optional\n");
+	printf("\nUsage: \033[32mphilo\033[0m ");
+	printf("\033[41m*\033[30;46mnumber_of_philosophers\033[45m:int\033[0m ");
+	printf("\033[41m*\033[30;46mtime_to_die\033[45m:int\033[0m ");
+	printf("\033[41m*\033[30;46mtime_to_eat\033[45m:int\033[0m ");
+	printf("\033[41m*\033[30;46mtime_to_sleep\033[45m:int\033[0m ");
+	printf("\033[30;43m?\033[46mnumber_of_times_each_philosopher_must_eat");
+	printf("\033[45m:int\033[0m\n");
+	printf("\n\t\033[31m<*>\033[0m - mandatory\n");
+	printf("\t\033[30;43m<?>\033[0m - optional\n");
 }
